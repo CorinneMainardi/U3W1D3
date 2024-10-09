be4288ebe268cc19fdfd21e740c4309a03d981c1
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-10-09 19:24:43 +0200

    restart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ActivePostsComponent } from './components/active-posts/active-posts.component';
-import { InactivePostsComponent } from './components/inactive-posts/inactive-posts.component';
-import { PostDetailComponent } from './components/post-detail/post-detail.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { InterfaceComponent } from './components/interface/interface.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ActivePostsComponent,
-    InactivePostsComponent,
-    PostDetailComponent,
-    NavbarComponent,
-    HomeComponent,
-    InterfaceComponent,
+    AppComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
