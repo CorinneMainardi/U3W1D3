bcd9bfc32daaee08e827d2a9015b0148f12c4836
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-10-10 16:48:24 +0200

    commit del 10/10

--- src/app/pages/post-detail/post-detail.component.ts
+++ src/app/pages/post-detail/post-detail.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iJSONresponse, iPost } from '../../Modules/i-post';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
-  styleUrl: './post-detail.component.scss'
+  styleUrl: './post-detail.component.scss',
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit {
+  post!: iPost;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: any) => {
+      fetch('db.json')
+        .then((response) => <Promise<iJSONresponse>>response.json())
+        .then((response) => {
+          const arrayPosts = response.posts;
+
+          const postDetail = arrayPosts.find((post) => post.id == params['id']);
+          if (postDetail) {
+            this.post = postDetail;
+            console.log(this.post);
+          } else {
+            console.log('trimone');
+          }
+        });
+    });
+  }
 }
